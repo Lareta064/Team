b831150d9a255c9763949cfa41a417a2eb51805c
Lareta064 <lareta064@gmail.com>
2023-02-17 20:34:52 +0300

    fix slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,66 @@
 document.addEventListener("DOMContentLoaded", function (){
-/*============= menu toggle ===============*/
+/*============= open/close mob menu ===============*/
   const menuToggle = document.querySelector('#menu-toggle');
  
   const mobileMenu = document.querySelector('#header-menu');
   const overlayBlock = document.querySelector('#overlay');
   
   const bodyEl = document.body;
-  
-  menuToggle.addEventListener('click', function (e) {
-    e.stopPropagation();
-    
-    if (this.classList.contains('active')) {
-       
-      this.classList.remove('active');
- 	   mobileMenu.classList.remove('active');
+  if(menuToggle){
+      menuToggle.addEventListener('click', function (e) {
+        e.stopPropagation();
+        
+        if (this.classList.contains('active')) {
+          
+          this.classList.remove('active');
+        mobileMenu.classList.remove('active');
 
-    } else {
-        this.classList.add('active');
-	      mobileMenu.classList.add('active');
-      	 bodyEl.classList.add('lock');
+        } else {
+            this.classList.add('active');
+            mobileMenu.classList.add('active');
+            bodyEl.classList.add('lock');
+          }
+        });
+
+        /*======== закрывать моб меню при ресайзе экрана ====== */
+        window.addEventListener('resize', function () {
+          if(this.innerWidth >1023){
+            if(mobileMenu.classList.contains('active')){
+              menuToggle.classList.remove('active');
+            //    mobileMenu.classList.remove('active');
+          
+              bodyEl.classList.remove('lock');
+            }
+          }      
+        });
+      }    
+    /* ================ swiper partners slider ==============*/
+    const swiperPartners = new Swiper('.partners-swiper', {
+      
+      slidesPerView: 1,
+      loop:true,
+      speed: 800,
+      navigation: {
+        nextEl: ".partners-swiper-next",
+        prevEl: ".partners-swiper-prev",
+      },
+      breakpoints: {
+      
+      575: {
+      slidesPerView: 2,
+     
+      },
+      
+      1024: {
+      slidesPerView: 3,
+     
+      },
+     
+      1536: {
+      slidesPerView: 4,
+      
       }
+    }
     });
 
-    /*======== закрывать моб меню при ресайзе экрана ====== */
-    window.addEventListener('resize', function () {
-      if(this.innerWidth >1023){
-        if(mobileMenu.classList.contains('active')){
-           menuToggle.classList.remove('active');
-        //    mobileMenu.classList.remove('active');
-      
-           bodyEl.classList.remove('lock');
-        }
-      }
-      
-    });
-
-    
 });
