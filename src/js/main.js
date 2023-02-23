27edbdb7bee7ac3bdad238e3d3f53b30fb4f951b
Lareta064 <lareta064@gmail.com>
2023-02-23 19:36:08 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -151,5 +151,28 @@ document.addEventListener("DOMContentLoaded", function (){
         $("#datepicker").datepicker();
       });
   }
+ /* =============== modal с атрибутом frame-modal ===============*/ 
+    const modalFramesOpen = document.querySelectorAll('[frame-btn]');
+    const modalFrames = document.querySelectorAll('[frame-modal]');
+    if( modalFrames.length > 0){
+      const modalFramesClose = document.querySelectorAll('[frame-close]');
 
+      for(let item of modalFramesOpen){
+        item.addEventListener('click', function(e){
+          for(let item of  modalFrames){
+          item.classList.remove('visible');
+           bodyEl.classList.remove('lock');
+          }
+          e.preventDefault();
+          const itemAttr = item.getAttribute('frame-btn');
+
+          for(let frame of modalFrames){
+            const frameAttr =frame.getAttribute('frame-modal');	
+            if(frameAttr == itemAttr){
+              frame.classList.add('visible');
+              bodyEl.classList.add('lock');
+            }
+          }
+        });
+      }
 });
