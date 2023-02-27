eb0aa23b6ff68a71b4a77b972aae90c5371b56e7
Lareta064 <lareta064@gmail.com>
2023-02-27 16:56:26 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -266,4 +266,66 @@ document.addEventListener("DOMContentLoaded", function (){
 			});
 		}
 	});
+
+	/** =============== custom select ===============*/
+	const mySelectBlocks = Array.from(document.getElementsByClassName('mySelect'));
+	if(mySelectBlocks.length > 0){
+		mySelectBlocks.forEach((item, i) =>{
+			const mySelect = item.querySelector('.mySelect-input');
+			const mySelectInput = item.querySelector('.selectValue');
+			let mySelectOptions = item.querySelectorAll('.mySelect-options');
+			// const mySelectIcon = item.querySelector('.mySelect-icon');
+			const mySelecDrop = item.querySelector('.mySelect-drop');
+
+			mySelect.addEventListener('click', ()=>{
+
+				if(mySelecDrop.classList.contains('active')){
+					mySelecDrop.classList.remove('active');
+					// mySelectIcon.classList.remove('active');
+					mySelect.classList.remove('open');
+
+
+				}else{
+					mySelecDrop.classList.add('active');
+					// mySelectIcon.classList.add('active');
+					mySelect.classList.add('open');
+				}
+
+			});
+			for(let item of mySelectOptions){
+				item.addEventListener('click', ()=>{
+					mySelecDrop.classList.remove('active');
+					mySelect.classList.remove('open');
+					// mySelectIcon.classList.remove('active');
+					mySelectInput.value = item.value;
+
+				});
+			}
+		});
+		
+	}	
+	/*========== закрыть mySelect по клику вне ===========*/
+	window.addEventListener('click', function(e){
+		
+		if (!e.target.closest('.mySelect')){
+			const mySelectOpenBlocks = Array.from(document.getElementsByClassName('mySelect'));
+			
+			if(mySelectOpenBlocks.length > 0){
+				mySelectOpenBlocks.forEach((item, i) =>{
+					const mySelect = item.querySelector('.mySelect-input');
+					const mySelectInput = item.querySelector('.selectValue');
+					let mySelectOptions = item.querySelectorAll('.mySelect-options');
+					// const mySelectIcon = item.querySelector('.mySelect-icon');
+					const mySelecDrop = item.querySelector('.mySelect-drop');
+					
+					mySelecDrop.classList.remove('active');
+					// mySelectIcon.classList.remove('active');
+					mySelect.classList.remove('open');
+				});
+			}
+		}
+		
+	});
+
+
 });
